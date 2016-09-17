de9d0e7f1512e1d64c975e45f6d653dd8086bc67
darshan <darshan.gowda008@gmail.com>
2016-09-17 18:16:20 +0530

    firebase dependency added

--- sailibrary/src/main/java/com/dark/sailibrary/Sailboat.java
+++ sailibrary/src/main/java/com/dark/sailibrary/Sailboat.java
@@ -1,9 +1,18 @@
 package com.dark.sailibrary;
 
+import com.google.firebase.database.FirebaseDatabase;
+
 /**
  * Created by Rohan on 9/17/2016.
  */
 public class Sailboat {
+
+
+
+    public Sailboat() {
+
+    }
+
 
 
 }

16e27d25ee803b3e174f1a7795a85b186fe98a7b
Rohan Kumar <rohan.carmel@gmail.com>
2016-09-17 20:39:50 +0530

    updating to firebase

--- app/src/main/java/com/dark/sail/MainActivity.java
+++ app/src/main/java/com/dark/sail/MainActivity.java
@@ -2,6 +2,13 @@ package com.dark.sail;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.dark.sailibrary.Sailboat;
+import com.dark.sailibrary.ViewObject;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<ViewObject> arrayList = new ArrayList<>();
+
+        arrayList.add(new ViewObject(Button.class,R.id.button));
+        arrayList.add(new ViewObject(TextView.class,R.id.text));
+
+        Sailboat sailboat = new Sailboat();
+        sailboat.initialize(MainActivity.this,arrayList);
     }
 }
--- sailibrary/src/main/java/com/dark/sailibrary/Sailboat.java
+++ sailibrary/src/main/java/com/dark/sailibrary/Sailboat.java
@@ -1,13 +1,18 @@
 package com.dark.sailibrary;
 
 import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by Rohan on 9/17/2016.
@@ -18,6 +23,7 @@ public class Sailboat {
     DatabaseReference databaseReference;
     Activity activity;
     ArrayList<ViewObject> listOfIds;
+    String TAG = "Test";
 
 
     public Sailboat() {
@@ -37,13 +43,54 @@ public class Sailboat {
 
     private void sendInfo() {
 
-        String packageName = activity.getPackageName().replaceAll("\\.","_");
-        String activityName = activity.getApplicationContext().getClass().getSimpleName();
-        for(int i=0;i<listOfIds.size();i++){
+        String packageName = activity.getPackageName().replaceAll("\\.", "_");
+        String activityName = activity.getClass().getSimpleName();
+
+        Log.d(TAG, "sendInfo: " + activityName);
+
+        HashMap<String, HashMap<String, String>> views = new HashMap<>();
+
+        for (int i = 0; i < listOfIds.size(); i++) {
             View v = (activity.findViewById(listOfIds.get(i).id));
-//            Object button = (listOfIds.get(i).aClass) v;
-//            button.getText();
+            String text;
+            try {
+                text = ((TextView) v).getHint().toString();
+            } catch (Exception e) {
+                text = ((TextView) v).getText().toString();
+                e.printStackTrace();
+            }
+
+            Log.d(TAG, "sendInfo: id " + listOfIds.get(i).id);
+            Log.d(TAG, "sendInfo: text " + text);
+            HashMap<String, String> hash = new HashMap<>();
+            hash.put("hint", text);
+            views.put("" + listOfIds.get(i).id, hash);
         }
+
+        HashMap<String, Object> activityHash = new HashMap<>();
+        activityHash.put("views", views);
+
+        HashMap<String, Object> packageHash = new HashMap<>();
+        packageHash.put(activityName, activityHash);
+
+//        HashMap<String,Object> rootHash = new HashMap<>();
+//        rootHash.put("activities",packageHash);
+//        packageHash.put("temp","HEllo");
+
+
+        databaseReference.child("packages/" + packageName + "/activities").setValue(packageHash)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d("TAG", "onSuccess: ");
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("TAG", "onFailure: " + e);
+            }
+        });
 
     }
 

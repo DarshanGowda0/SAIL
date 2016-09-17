ddb736f8736afa15b4bb90050994e55a5069be53
darshan <darshan.gowda008@gmail.com>
2016-09-17 19:10:56 +0530

    firebase dependency added

--- sailibrary/src/main/java/com/dark/sailibrary/Sailboat.java
+++ sailibrary/src/main/java/com/dark/sailibrary/Sailboat.java
@@ -1,18 +1,51 @@
 package com.dark.sailibrary;
 
+import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 /**
  * Created by Rohan on 9/17/2016.
  */
 public class Sailboat {
 
+    FirebaseDatabase database;
+    DatabaseReference databaseReference;
+    Activity activity;
+    ArrayList<ViewObject> listOfIds;
 
 
     public Sailboat() {
 
+        database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference();
+
     }
 
+    public void initialize(Activity activity, ArrayList<ViewObject> listOfIds) {
+        this.activity = activity;
+        this.listOfIds = listOfIds;
+
+        sendInfo();
+
+    }
+
+    private void sendInfo() {
+
+        String packageName = activity.getPackageName().replaceAll("\\.","_");
+        String activityName = activity.getApplicationContext().getClass().getSimpleName();
+        for(int i=0;i<listOfIds.size();i++){
+            View v = (activity.findViewById(listOfIds.get(i).id));
+//            Object button = (listOfIds.get(i).aClass) v;
+//            button.getText();
+        }
+
+    }
 
 
 }
---
+++ sailibrary/src/main/java/com/dark/sailibrary/ViewObject.java
@@ -0,0 +1,15 @@
+package com.dark.sailibrary;
+
+/**
+ * Created by darshan on 17/09/16.
+ */
+public class ViewObject {
+
+    Class aClass;
+    Integer id;
+
+    public ViewObject(Class aClass, Integer id) {
+        this.aClass = aClass;
+        this.id = id;
+    }
+}
